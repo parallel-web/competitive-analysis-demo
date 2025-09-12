8c934ec8226a916aad953ae4fc393148fcb31c55
Jan Wilmake <jan@wilmake.com>
2025-09-12 15:10:34 +0200

    update schema change date

--- worker.ts
+++ worker.ts
@@ -20,7 +20,7 @@ import resultHtml from "./result.html";
 const ANALYSIS_LIMIT = 5;
 const DO_NAME = "v5";
 const ADMIN_USERNAMES = ["janwilmake", "khushi_shelat"];
-const NEW_VERSION_DATE = 1757517150949;
+const NEW_VERSION_DATE = 1757682596716;
 
 export interface Env {
   COMPETITOR_ANALYSIS: DurableObjectNamespace<
